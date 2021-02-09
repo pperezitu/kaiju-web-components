6903373abda4bbd0bb2b668116913373c07b682a
Pedro <landscape@Pedro-Navaja.local>
2021-02-09 17:42:57 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# KaijuWebComponents
+
+Libreria base de componentes web construida solo con Javascript
+
+# Servidor de Desarrollo
+
+Run `npm run start` for a dev server. Navigate to `http://localhost:3000/`. The app will automatically reload if you change any of the source files.
---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname + "/public/index.html"));
+});
+
+app.listen(3000, () => {
+    console.log("Servidor corriendo en el puerto", 3000);
+});
