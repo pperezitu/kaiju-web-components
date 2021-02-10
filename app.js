d9069aad11dcb678bccc423b89c1a7a2c98e6933
Pedro <landscape@Pedro-Navaja.local>
2021-02-10 08:33:36 -0300

    se agrega componente boton

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname + "/public/index.html"));
 });
 
+app.use(express.static(path.join(__dirname, "/public")));
+
 app.listen(3000, () => {
     console.log("Servidor corriendo en el puerto", 3000);
 });
---
+++ public/components/kj-button.js
@@ -0,0 +1,47 @@
+class kaijuButton extends HTMLElement {
+
+    get template() {
+        return `
+            <style>
+                div {
+                    display: inline-block;
+                    color: #fff;
+                    padding: 10px;
+                    cursor: pointer;
+                    background: #000;
+                }
+            </style>
+
+            <div class="$(this.status)">
+                <slot></slot>
+            </div>
+        `
+    };
+
+    constructor(){
+        super();
+        let currentStatus = this.getAttribute('status');
+        if(currentStatus) {
+            this.status = currentStatus;
+        } else {
+            this.status = 'neutral';
+        }
+        let shadowRoot = this.attachShadow({mode : 'open'});
+        shadowRoot.innerHTML = this.template;
+    };
+
+    static get observedAttributes() {
+        return ['status'];
+    }
+    attributeChangeCallback(attr, oldVal, newVal) {
+        console.log('attributeChangedCallback');
+        if(attr == 'status' && oldVal != newVal) {
+            this.status = newVal;
+            console.log(this.status);
+            this.shadowRoot.innerHTML = this.template;
+        }
+    }
+
+}
+
+window.customElements.define('kj-button', kaijuButton);
